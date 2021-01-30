16bcf380cd258028e5aee66ece0a0fa5755f1c28
MoonKnight2020 <jamescbryantii@gmail.com>
2021-01-29 22:38:21 -0500

    Added more Queries

--- data/Employees_db.sql
+++ data/Employees_db.sql
@@ -61,3 +61,48 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
+FROM employees, salaries 
+WHERE employees.emp_no = salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date 
+FROM employees 
+WHERE employees.hire_date LIKE '%1986';
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name 
+FROM employees, departments, dept_manager  
+WHERE employees.emp_no = dept_manager.emp_no AND dept_manager.dept_no = departments.dept_no;
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees, departments, dept_emp
+WHERE employees.emp_no = dept_emp.emp_no AND dept_emp.dept_no = departments.dept_no;
+
+--List first name, last name, and sex for employees whose 
+--first name is "Hercules" and last names begin with "B."
+SELECT employees.first_name, employees.last_name, employees.sex 
+FROM employees
+WHERE employees.first_name LIKE 'Hercules' AND employees.last_name LIKE 'B%';
+
+--List all employees in the Sales department, 
+--including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees, departments
+WHERE departments.dept_no = 'd007';
+
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees, departments
+WHERE departments.dept_no = 'd007' OR departments.dept_no = 'd005';
+
+--In descending order, 
+--list the frequency count of employee last names, i.e., how many employees share each last name
+SELECT COUNT(employees.last_name) from employees
+select * from departments
